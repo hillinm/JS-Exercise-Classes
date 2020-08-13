9bb9e76cfd766492ed59feee15f42c789d0eec63
Mark Hillin <mark.hillin@gmail.com>
2020-08-13 13:42:32 -0500

    Task 2 Completed

--- index.js
+++ index.js
@@ -73,7 +73,26 @@ class Person {
 */
 
 class Car {
-
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank += gallons;
+  }
+  drive(distance){
+    let maxdistance = this.tank * this.milesPerGallon;
+    if (distance >= maxdistance) {
+      this.odometer += maxdistance;
+      this.tank -= (maxdistance/this.milesPerGallon);
+      return `I ran out of fuel at ` + this.odometer + ` miles`;
+    } else if (maxdistance > distance) {
+      this.odometer += distance;
+      this.tank -= (distance/this.milesPerGallon);
+    }
+  }
 }
 
 /*
